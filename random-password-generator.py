43a2caf470a1ead5102a09969b245a66f6916386
ali <chatroom23456@gmail.com>
2022-05-05 06:07:36 -0400

    add lowercase alphabet and uppercase alphabet

--- random-password-generator.py
+++ random-password-generator.py
@@ -1,11 +1,16 @@
 #python3
 import random, string
 
+char_finally = ""
 len_number = int(input("Enter your password length : "))
-list_number = string.digits
+use_number = string.digits if input("use number ? (Y, N): ") in "yY" else ""
+use_lowercase_alphabet = string.ascii_lowercase if input("use lowercase alphabet? (Y, N): ") in "yY" else ""
+use_uppercase_alphabet = string.ascii_uppercase if input("use uppercase alphabet? (Y, N): ") in "yY" else ""
+
+char_finally += use_number + use_lowercase_alphabet + use_uppercase_alphabet
 
 for number in range(len_number):
     if number+1 == len_number:
-        print(random.choice(list_number))
+        print(random.choice(char_finally))
     else:
-        print(random.choice(list_number), end="")
+        print(random.choice(char_finally), end="")
