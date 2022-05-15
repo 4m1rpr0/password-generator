51f53bca156aad2581ca5167f45d3d2f0b049c82
ali <chatroom23456@gmail.com>
2022-05-15 05:38:26 -0400

    Use user's words

--- random-password-generator.py
+++ random-password-generator.py
@@ -13,8 +13,22 @@ use_symbols = string.punctuation if input("use symbols? (Y, N): ") in "yY" else 
 
 char_finally += use_number + use_lowercase_alphabet + use_uppercase_alphabet + use_symbols
 
+
+while True:
+    use_add = input("are you need add char in char list ('show' show list char, Y, N) : ")
+    if use_add in "Yy":
+        char_finally += input("enter your chars or symbols : ")
+        break
+    if use_add.lower() == "show":
+        print(char_finally)
+        continue
+    else:
+        break
+
+
 for number in range(len_number):
     if number+1 == len_number:
         print(random.choice(char_finally))
     else:
         print(random.choice(char_finally), end="")
+print(char_finally)
